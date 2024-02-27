157af5b90ae5da84dfd231da11af8e1140cf0c14
Elizar54 <y.elizarev@g.nsu.ru>
2024-02-27 11:51:34 +0700

    deleted comments

--- main.py
+++ main.py
@@ -44,33 +44,33 @@ def find_short_queue(avl_lst, aut_lst):
     return min_que_col
 
 
-clients = [] # Все клиенты
-missed_clients = [] # Потерянные клиенты
-automats = [] # Основная инфа по автоматам
-arr_time_lst = [] # время прибытия клиентов
-gas_price = {ru.ai_80: 49, ru.ai_92: 50, ru.ai_95: 55, ru.ai_98: 68} # цены на бензин
-gas_avail = {} # на каких колонках доступна каждая марка топлива
-cash = 0 # выручка
-sold_gas = {ru.ai_80: 0, ru.ai_92: 0, ru.ai_95: 0, ru.ai_98: 0} # объем проданного топлива по маркам
-time_deoccup = {} # время, в которое освобождается каждый автомат в форме словаря
-column_queue = {} # очередь с информацией о клиентах у каждой колонки
-current_client = {} # клиенты, которые обслуживаются в данный момент у колонки
+clients = []
+missed_clients = [] 
+automats = [] 
+arr_time_lst = [] 
+gas_price = {ru.ai_80: 49, ru.ai_92: 50, ru.ai_95: 55, ru.ai_98: 68} 
+gas_avail = {} 
+cash = 0 
+sold_gas = {ru.ai_80: 0, ru.ai_92: 0, ru.ai_95: 0, ru.ai_98: 0}
+time_deoccup = {} 
+column_queue = {} 
+current_client = {} 
 
 with open('input_automats.txt', encoding='utf-8') as file:
     for x in file:
         automat = {}
         seq = x.split()
-        automat[ru.aut_num] = seq[0] # заполняем словарь по каждому автомату и формируем список из таких словарей
+        automat[ru.aut_num] = seq[0]
         automat[ru.queue_max] = seq[1]
         automat[ru.gas_types] = seq[2:]
         automat[ru.queue] = 0
         automats.append(automat)
 
-        time_deoccup[seq[0]] = None # создаем ключ для каждого автомата в следующих трех словарях
+        time_deoccup[seq[0]] = None 
         column_queue[seq[0]] = []
         current_client[seq[0]] = None
 
-        for elem in automat[ru.gas_types]: # формируем словарь, в котором будет информация о том, у какого автомата доступна каждая марка бензина
+        for elem in automat[ru.gas_types]: 
             if elem in gas_avail:
                 gas_avail[elem].append(seq[0])
             else:
@@ -79,29 +79,30 @@ with open('input_automats.txt', encoding='utf-8') as file:
 
 with open('input_clients.txt', encoding='utf-8') as file:
     for x in file:
-        clients.append(tuple(x.split())) # добавляем кортеж с инфой о клиенте в список
+        clients.append(tuple(x.split())) 
 
 for client in clients:
-    arr_time_lst.append(client[0]) # создаем список, содержащий время прибытия каждого клиента
+    arr_time_lst.append(client[0]) 
 
-for mnt in range(1440): # главный цикл
-    curr_time = translate_time(mnt) # переводим минуты в часы
+for mnt in range(1440): 
+    curr_time = translate_time(mnt) 
 
-    for k, v in time_deoccup.items(): # идем по словарю освобождения колонок
-        if curr_time == v: # если данная минута совпадает с одним из значений словаря освобождения колонки, то запускается следующий код
-            automats[int(k) - 1][ru.queue] -= 1 # уменьшаем очередь на единицу
+    for k, v in time_deoccup.items(): 
+        if curr_time == v: 
+            automats[int(k) - 1][ru.queue] -= 1
             client_left_out = ''
-            
+
             for x in current_client[automats[int(k) - 1][ru.aut_num]]:
                 client_left_out += x + ' '
+
             print(f'{ru.client_up} {client_left_out}{ru.left_at} {curr_time}')
             print()
             
             #time.sleep(0.3)
 
-            if automats[int(k) - 1][ru.queue] > 0: # смотрим, есть ли кто в очереди к этой колонке
-                next_client_demand = column_queue[k][0][1] # смотрим, сколько литров нужно следующему клиенту
-                next_deoccup_time = client_time(next_client_demand, mnt)[0] # считаем время освобождения колонки этим клиентом
+            if automats[int(k) - 1][ru.queue] > 0: 
+                next_client_demand = column_queue[k][0][1] 
+                next_deoccup_time = client_time(next_client_demand, mnt)[0] 
                 time_deoccup[k] = next_deoccup_time # заменяем значение в словаре на время нового клиента
 
                 fueling_start_out = ''
@@ -117,8 +118,8 @@ for mnt in range(1440): # главный цикл
                 time_deoccup[k] = 0
             
 
-    if curr_time in set(arr_time_lst): # смотрим, прибывает ли новый клиент в это время
-        client = clients[arr_time_lst.index(curr_time)] # смотрим, что за клиент
+    if curr_time in set(arr_time_lst): 
+        client = clients[arr_time_lst.index(curr_time)] 
         time_for_client = client_time(client[1], mnt) 
         client_new_out = ''
 
@@ -127,14 +128,14 @@ for mnt in range(1440): # главный цикл
 
         print(f'{ru.at} {curr_time} {ru.new_client} {client_new_out}{time_for_client[1]}', end=' ')
 
-        gas_type = client[2] # смотрим, какой бензин ему нужен
+        gas_type = client[2] 
         
         client_column = find_short_queue(gas_avail[gas_type], automats)
 
-        if client_column != '': # если нашлось место в очереди
-            automats[int(client_column) - 1][ru.queue] += 1 # увеличиеваем очередь
-            cash += int(client[1]) * gas_price[client[2]] # кэш на базе
-            sold_gas[client[2]] += int(client[1]) # увеличиваем количество проданного топлива
+        if client_column != '': 
+            automats[int(client_column) - 1][ru.queue] += 1 
+            cash += int(client[1]) * gas_price[client[2]] 
+            sold_gas[client[2]] += int(client[1]) 
             #time.sleep(0.2)
 
             if automats[int(client_column) - 1][ru.queue] == 1: 
@@ -149,7 +150,7 @@ for mnt in range(1440): # главный цикл
             msg = f'{ru.queue_entry}{msg_out}'
             print(msg)
 
-        else:  # если колонка не нашлась
+        else:  
             missed_clients.append(client)
             print(ru.client_left)
 
